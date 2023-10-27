47b316f23a040e136717658e6e4892da80fc91c5
Shubh Agrahari <108170423+Shubh-6657@users.noreply.github.com>
2023-10-27 12:00:16 +0530

    Dropdown.js

--- src/Dropdown.js
+++ src/Dropdown.js
@@ -12,12 +12,37 @@ import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
+import Tooltip from '@mui/material/Tooltip';
+import Avatar from '@mui/material/Avatar';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Badge from '@mui/material/Badge';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import SearchIcon from '@mui/icons-material/Search';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import SettingsIcon from '@mui/icons-material/Settings';
+import ForumIcon from '@mui/icons-material/Forum';
+import MenuOpenIcon from '@mui/icons-material/MenuOpen';
+import img2 from './images/Logo.png';
+import Collapse from '@mui/material/Collapse';
+import ExpandLess from '@mui/icons-material/ExpandLess';
+import ExpandMore from '@mui/icons-material/ExpandMore';
+import StarBorder from '@mui/icons-material/StarBorder';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import FireTruckIcon from '@mui/icons-material/FireTruck';
+import AirportShuttleIcon from '@mui/icons-material/AirportShuttle';
+import PersonIcon from '@mui/icons-material/Person';
+import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
+import TextSnippetIcon from '@mui/icons-material/TextSnippet';
+import GavelIcon from '@mui/icons-material/Gavel';
+import LocalGasStationIcon from '@mui/icons-material/LocalGasStation';
+import BookmarkAddedIcon from '@mui/icons-material/BookmarkAdded';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import Link from '@mui/material/Link';
+
 
 const drawerWidth = 240;
 
@@ -47,7 +72,6 @@ const DrawerHeader = styled('div')(({ theme }) => ({
   alignItems: 'center',
   justifyContent: 'flex-end',
   padding: theme.spacing(0, 1),
-  // necessary for content to be below app bar
   ...theme.mixins.toolbar,
 }));
 
@@ -86,9 +110,22 @@ const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' 
   }),
 );
 
+
 export default function MiniDrawer() {
   const theme = useTheme();
   const [open, setOpen] = React.useState(false);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+  const settings = ['Setting 1', 'Setting 2', 'Setting 3']; 
+  const [open1, setOpen1] = React.useState(true);
+  const [open2, setOpen2] = React.useState(true);
+  const [open3, setOpen3] = React.useState(true);
+  const [open4, setOpen4] = React.useState(true);
+  const [open5, setOpen5] = React.useState(true);
+  const [open6, setOpen6] = React.useState(true);
+  const [open7, setOpen7] = React.useState(true);
+  const [open8, setOpen8] = React.useState(true);
+  const [open9, setOpen9] = React.useState(true);
+  const [open10, setOpen10] = React.useState(true);
 
   const handleDrawerOpen = () => {
     setOpen(true);
@@ -98,11 +135,60 @@ export default function MiniDrawer() {
     setOpen(false);
   };
 
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  const handleClick1 = () => {
+    setOpen1(!open1);
+  };
+
+  const handleClick2 = () => {
+    setOpen2(!open2);
+  };
+
+  const handleClick3 = () => {
+    setOpen3(!open3);
+  };
+
+  const handleClick4 = () => {
+    setOpen4(!open4);
+  };
+
+  const handleClick5 = () => {
+    setOpen5(!open5);
+  };
+
+  const handleClick6 = () => {
+    setOpen6(!open6);
+  };
+
+  const handleClick7 = () => {
+    setOpen7(!open7);
+  };
+
+  const handleClick8 = () => {
+    setOpen8(!open8);
+  };
+
+  const handleClick9 = () => {
+    setOpen9(!open9);
+  };
+
+  const handleClick10 = () => {
+    setOpen10(!open10);
+  };
+
   return (
     <Box sx={{ display: 'flex' }}>
       <CssBaseline />
       <AppBar position="fixed" open={open}>
-        <Toolbar>
+             {/* <---------------  Starting of NavBar------------>*/}
+        <Toolbar sx={{ justifyContent: 'space-between' }}>
           <IconButton
             color="inherit"
             aria-label="open drawer"
@@ -113,73 +199,285 @@ export default function MiniDrawer() {
               ...(open && { display: 'none' }),
             }}
           >
-            <MenuIcon />
+          <MenuIcon />
           </IconButton>
           <Typography variant="h6" noWrap component="div">
-            Mini variant drawer
+            <ul style={{ display: 'flex', listStyle: 'none', padding: '0'}}>
+            </ul>
           </Typography>
-        </Toolbar>
-      </AppBar>
-      <Drawer variant="permanent" open={open}>
-        <DrawerHeader>
-          <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+          <Box sx={{ flexGrow: 0 }}>
+          <IconButton type="button"aria-label="search" sx={{ fontSize: '36px', padding: '10px' }} color="inherit">
+            <SearchIcon />
           </IconButton>
-        </DrawerHeader>
-        <Divider />
-        <List>
-          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-            <ListItem key={text} disablePadding sx={{ display: 'block' }}>
-              <ListItemButton
-                sx={{
-                  minHeight: 48,
-                  justifyContent: open ? 'initial' : 'center',
-                  px: 2.5,
-                }}
-              >
-                <ListItemIcon
-                  sx={{
-                    minWidth: 0,
-                    mr: open ? 3 : 'auto',
-                    justifyContent: 'center',
-                  }}
-                >
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+          <IconButton type="button"aria-label="search" sx={{ fontSize: '36px', padding: '10px' }} color="inherit">
+            <CalendarMonthIcon />
+          </IconButton>
+          <IconButton type="button"aria-label="search" sx={{ fontSize: '36px', padding: '10px' }} color="inherit">
+            <SettingsIcon />
+          </IconButton>
+          <IconButton  sx={{ fontSize: '36px', padding: '16px' }} aria-label="show 1 new notifications" color="inherit">
+            <Badge badgeContent={1} color="error">
+              <NotificationsIcon />
+            </Badge>
+          </IconButton>
+          <IconButton type="button"aria-label="search" sx={{ fontSize: '36px', padding: '10px' }} color="inherit">
+            <ForumIcon />
+          </IconButton>
+          <IconButton type="button"aria-label="search" sx={{ fontSize: '36px', padding: '10px' }} color="inherit">
+            <MenuOpenIcon />
+          </IconButton>
+          <Tooltip title="Open settings">
+          <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+            <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" sx={{ width: 32, height: 32 }} />
+          </IconButton>
+          </Tooltip>
+                      {/* <---------------  Ending of NavBar------------>*/}
+
+                      {/* <---------------  Starting of Drawer header Tech Trukers Image------------>*/}
+
+          <Menu
+            sx={{ mt: '45px' }}
+            id="menu-appbar"
+            anchorEl={anchorElUser}
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            open={Boolean(anchorElUser)}
+            onClose={handleCloseUserMenu}
+          >
+            {settings.map((setting) => (
+              <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                <Typography textAlign="center">{setting}</Typography>
+              </MenuItem>
+            ))}
+          </Menu>
+          </Box>
+          </Toolbar> 
+      </AppBar>
+          <Drawer variant="permanent" open={open}>
+          <DrawerHeader>
+          <div>
+            <img
+              src={img2}
+              style={{height:'48px',width:'188px'}}
+              alt="Dynamic Portage Salarial"
+            />
+          </div>
+          <IconButton onClick={handleDrawerClose}>
+              {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+          </IconButton>
+          </DrawerHeader> 
+
+                    {/* <---------------  Ending of Drawer header Tech Trukers Image------------>*/}
+          <Divider />
+                    
+                    {/* <---------------  Starting of SideBar(DashBoard ,Dispatch etc )------------>*/}
+        <List> 
+          <Box sx={{ typography: 'body1' }}>
+          <ListItemButton onClick={handleClick1}>
+                    <ListItemIcon>
+                    <DashboardIcon />
+                  </ListItemIcon>
+                  <Link href="/"><ListItemText primary="Dashboard" /></Link>
+                  {open1 ? <ExpandLess /> : <ExpandMore />}
+                </ListItemButton>
+                </Box>
+                <Collapse in={open1} timeout="auto" unmountOnExit>
+                  <List component="div" disablePadding>
+                    <ListItemButton sx={{ pl: 4 }}>
+                      <ListItemIcon>
+                        <StarBorder />
+                      </ListItemIcon>
+                      <ListItemText primary="Starred" />
+                    </ListItemButton>
+                  </List>
+                </Collapse>
+
+
+                <Box sx={{ typography: 'body1' }}>
+              <ListItemButton onClick={handleClick2}>
+                <ListItemIcon>
+                  <FireTruckIcon />
                 </ListItemIcon>
-                <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
+                <Link href="/"><ListItemText primary="Dispatch Board" /></Link>
+                {open2 ? <ExpandLess /> : <ExpandMore />}
               </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-        <Divider />
-        <List>
-          {['All mail', 'Trash', 'Spam'].map((text, index) => (
-            <ListItem key={text} disablePadding sx={{ display: 'block' }}>
-              <ListItemButton
-                sx={{
-                  minHeight: 48,
-                  justifyContent: open ? 'initial' : 'center',
-                  px: 2.5,
-                }}
-              >
-                <ListItemIcon
-                  sx={{
-                    minWidth: 0,
-                    mr: open ? 3 : 'auto',
-                    justifyContent: 'center',
-                  }}
-                >
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </Box>
+              <Collapse in={open2} timeout="auto" unmountOnExit>
+                <List component="div" disablePadding>
+                  <ListItemButton sx={{ pl: 4 }}>
+                    <ListItemIcon>
+                      <StarBorder />
+                    </ListItemIcon>
+                    <ListItemText primary="Starred" />
+                  </ListItemButton>
+                </List>
+              </Collapse>
+
+
+              <ListItemButton onClick={handleClick3}>
+                <ListItemIcon>
+                  <AirportShuttleIcon />
                 </ListItemIcon>
-                <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
+                <Link href="/"><ListItemText primary="Vehicle" /></Link>
+                {open3 ? <ExpandLess /> : <ExpandMore />}
               </ListItemButton>
-            </ListItem>
-          ))}
+              <Collapse in={open3} timeout="auto" unmountOnExit>
+                <List component="div" disablePadding>
+                  <ListItemButton sx={{ pl: 4 }}>
+                    <ListItemIcon>
+                      <StarBorder />
+                    </ListItemIcon>
+                    <ListItemText primary="Starred" />
+                  </ListItemButton>
+                </List>
+              </Collapse>
+
+
+              <ListItemButton onClick={handleClick4}>
+                <ListItemIcon>
+                  <PersonIcon />
+                </ListItemIcon>
+                <ListItemText primary="Drivers" />
+                {open4 ? <ExpandLess /> : <ExpandMore />}
+              </ListItemButton>
+              <Collapse in={open4} timeout="auto" unmountOnExit>
+                <List component="div" disablePadding>
+                  <ListItemButton sx={{ pl: 4 }}>
+                    <ListItemIcon>
+                      <StarBorder />
+                    </ListItemIcon>
+                    <ListItemText primary="Starred" />
+                  </ListItemButton>
+                </List>
+              </Collapse>
+
+
+              <ListItemButton onClick={handleClick5}>
+                <ListItemIcon>
+                  <AccountBalanceWalletIcon />
+                </ListItemIcon>
+                <ListItemText primary="Accounting" />
+                {open5 ? <ExpandLess /> : <ExpandMore />}
+              </ListItemButton>
+              <Collapse in={open5} timeout="auto" unmountOnExit>
+                <List component="div" disablePadding>
+                  <ListItemButton sx={{ pl: 4 }}>
+                    <ListItemIcon>
+                      <StarBorder />
+                    </ListItemIcon>
+                    <ListItemText primary="Starred" />
+                  </ListItemButton>
+                </List>
+              </Collapse>
+
+
+              <ListItemButton onClick={handleClick6}>
+                <ListItemIcon>
+                  <TextSnippetIcon />
+                </ListItemIcon>
+                <ListItemText primary="Reports" />
+                {open6 ? <ExpandLess /> : <ExpandMore />}
+              </ListItemButton>
+              <Collapse in={open6} timeout="auto" unmountOnExit>
+                <List component="div" disablePadding>
+                  <ListItemButton sx={{ pl: 4 }}>
+                    <ListItemIcon>
+                      <StarBorder />
+                    </ListItemIcon>
+                    <ListItemText primary="Starred" />
+                  </ListItemButton>
+                </List>
+              </Collapse>
+
+
+              <ListItemButton onClick={handleClick7}>
+                <ListItemIcon>
+                  <GavelIcon />
+                </ListItemIcon>
+                <ListItemText primary="Compliance" />
+                {open7 ? <ExpandLess /> : <ExpandMore />}
+              </ListItemButton>
+              <Collapse in={open7} timeout="auto" unmountOnExit>
+                <List component="div" disablePadding>
+                  <ListItemButton sx={{ pl: 4 }}>
+                    <ListItemIcon>
+                      <StarBorder />
+                    </ListItemIcon>
+                    <ListItemText primary="Starred" />
+                  </ListItemButton>
+                </List>
+              </Collapse>
+
+
+              <ListItemButton onClick={handleClick8}>
+                <ListItemIcon>
+                  <LocalGasStationIcon />
+                </ListItemIcon>
+                <ListItemText primary="IFTA" />
+                {open8 ? <ExpandLess /> : <ExpandMore />}
+              </ListItemButton>
+              <Collapse in={open8} timeout="auto" unmountOnExit>
+                <List component="div" disablePadding>
+                  <ListItemButton sx={{ pl: 4 }}>
+                    <ListItemIcon>
+                      <StarBorder />
+                    </ListItemIcon>
+                    <ListItemText primary="Starred" />
+                  </ListItemButton>
+                </List>
+              </Collapse>
+
+
+              <ListItemButton onClick={handleClick9}>
+                <ListItemIcon>
+                  <BookmarkAddedIcon />
+                </ListItemIcon>
+                <ListItemText primary="Inspection" />
+                {open9 ? <ExpandLess /> : <ExpandMore />}
+              </ListItemButton>
+              <Collapse in={open9} timeout="auto" unmountOnExit>
+                <List component="div" disablePadding>
+                  <ListItemButton sx={{ pl: 4 }}>
+                    <ListItemIcon>
+                      <StarBorder />
+                    </ListItemIcon>
+                    <ListItemText primary="Starred" />
+                  </ListItemButton>
+                </List>
+              </Collapse>
+
+              <ListItemButton onClick={handleClick10}>
+                <ListItemIcon>
+                  <MoreHorizIcon />
+                </ListItemIcon>
+                <ListItemText primary="More" />
+                {open10 ? <ExpandLess /> : <ExpandMore />}
+              </ListItemButton>
+              <Collapse in={open10} timeout="auto" unmountOnExit>
+                <List component="div" disablePadding>
+                  <ListItemButton sx={{ pl: 4 }}>
+                    <ListItemIcon>
+                      <StarBorder />
+                    </ListItemIcon>
+                    <ListItemText primary="Starred" />
+                  </ListItemButton>
+                </List>
+              </Collapse>
         </List>
+                               {/* https://mui.com/material-ui/react-list/ */}
+                   {/* <---------------  Ending of SideBar(DashBoard ,Dispatch etc )------------>*/}
+       <Divider />
       </Drawer>
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
         <DrawerHeader />
-        <Typography paragraph>
+        <Typography paragraph variant="body1">
          Welcome to the
         </Typography>
         <Typography paragraph>
