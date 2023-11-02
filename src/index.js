f4f0b32ffead67da253d78342bdb17e33bc818a9
Shubh Agrahari <108170423+Shubh-6657@users.noreply.github.com>
2023-11-02 16:03:59 +0530

    index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { StyledEngineProvider } from '@mui/material/styles';
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
      <StyledEngineProvider injectFirst>
+      <BrowserRouter>
     <App />
+    </BrowserRouter>
     </StyledEngineProvider>
   </React.StrictMode>
 );
