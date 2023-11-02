7b4c666c8b84a4bd96384243f455acacbf19ca31
Shubh Agrahari <108170423+Shubh-6657@users.noreply.github.com>
2023-11-02 16:04:36 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import React from 'react';
-import Navbar from './Navbar';
+// import Navbar from './Navbar';
 import Dropdown from './Dropdown';
+import Dashboard from '@mui/icons-material/Dashboard';
+import { Route,Routes } from 'react-router-dom';
+// import { Dashboarda } from './pages/Dashboard';
+// import { Dashboard } from '@mui/icons-material';
+
 
 function App() {
   return (
     <div className="App">
       <header>
-        <Navbar />
+        {/* <Navbar /> */}
         <Dropdown />
       </header>
-      <main>
-      </main>
+      {/* <Dashboard/> */}
+      <nav>
+        <ul>
+          <li><a href="/Dashboarda">Dashboard</a></li>
+        </ul>
+      </nav>
+      <Routes>
+      <Route path="/Dashboarda" element={<Dashboard/>} />
+    </Routes>
+
     </div>
+
+
   );
 }
 
