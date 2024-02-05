d51666f1aee419b4cd8674c87296b10825f974fa
Shubh Agrahari <108170423+Shubh-6657@users.noreply.github.com>
2024-02-05 17:26:38 +0530

    Dashboard.js

--- pages/Dashboard.js
+++ pages/Dashboard.js
@@ -1,52 +1,17 @@
 import * as React from 'react';
-import { useTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
-import List from '@mui/material/List';
-import CssBaseline from '@mui/material/CssBaseline';
-import DashboardIcon from '@mui/icons-material/Dashboard';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import ExpandLess from '@mui/icons-material/ExpandLess';
-import ExpandMore from '@mui/icons-material/ExpandMore';
-import Collapse from '@mui/material/Collapse';
-import StarBorder from '@mui/icons-material/StarBorder';
+import Typography from '@mui/material/Typography';
 
-export function Dashboarda() {
-    const theme = useTheme();
-    const [open1, setOpen1] = React.useState(true);
+export default function DashBoard() {
 
-      const handleClick1 = () => {
-        setOpen1(!open1);
-      };
-
-return (
-    <Box sx={{ display: 'flex' }}>
-    <CssBaseline />
-
-    <List> 
-
-<ListItemButton onClick={handleClick1}>
-          <ListItemIcon>
-          <DashboardIcon />
-        </ListItemIcon>
-        <ListItemText primary="Dashboard" />
-        {open1 ? <ExpandLess /> : <ExpandMore />}
-      </ListItemButton>
-      <Collapse in={open1} timeout="auto" unmountOnExit>
-        <List component="div" disablePadding>
-          <ListItemButton sx={{ pl: 4 }}>
-            <ListItemIcon>
-              <StarBorder />
-            </ListItemIcon>
-            <ListItemText primary="Starred" />
-          </ListItemButton>
-        </List>
-      </Collapse>
-
-      </List>
-
-      </Box>
-
-);
+    return (
+    <Box component="main" sx={{ flexGrow: 1, p: 3 ,marginLeft: '50px'}}>
+    <Typography paragraph variant="body1">
+    Welcome to the
+    </Typography>
+    <Typography paragraph>
+    Dashboard Portion
+    </Typography>
+    </Box>
+    )
 }
